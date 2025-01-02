1b4f46c8c76f47abf6e48ba70e99ad5180f871bb
Oqisu_req <rojok3001@gmail.com>
2025-01-03 02:11:11 +0300

    Created glass like login div

---
+++ web/front/src/login.jsx
@@ -0,0 +1,34 @@
+import './login.css'
+
+function LogIn() {
+  return (
+    <>
+    <div class="wrapper">
+      <form action="#">
+        <h2>Login</h2>
+        <div class="input-field">
+          <input type="text" required></input>
+          <label>Enter your email</label>
+        </div>
+        <div class="input-field">
+          <input type="password" required></input>
+          <label>Enter your password</label>
+        </div>
+        <div class="forget">
+          <label for="remember">
+            <input type="checkbox" id="remember"></input>
+            <p>Remember me</p>
+          </label>
+          <a href="#">Forgot password?</a>
+        </div>
+        <button type="submit">Log In</button>
+        <div class="register">
+          <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+      </form>
+    </div>
+    </>
+    )
+}
+
+export default LogIn
--- web/front/src/main.jsx
+++ web/front/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import LogIn from './login.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('login')).render(
   <StrictMode>
-    <App />
+    <LogIn />
   </StrictMode>,
 )
