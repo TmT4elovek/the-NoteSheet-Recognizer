4acad4db91533eda13f82367730a6068084adc2f
Oqisu_req <rojok3001@gmail.com>
2025-02-09 19:30:11 +0300

    user auth and log out

--- web/front/front_route.py
+++ web/front/front_route.py
@@ -17,7 +17,6 @@ config = AuthXConfig()
 config.JWT_SECRET_KEY = 'secret'
 config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
 config.JWT_TOKEN_LOCATION = ["cookies"]
-# config.JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=120)
 
 authx = AuthX(config)
 
@@ -26,32 +25,40 @@ authx = AuthX(config)
 async def register(request: Request):
     return templates.TemplateResponse(request, 'register.html', {'request': request})
 
-
 @front.get('/login')
 async def login(request: Request):
     return templates.TemplateResponse(request, 'login.html', {'request': request})
 
 @front.get('/')
 async def home(request: Request):
-    return templates.TemplateResponse(request, 'home.html', {'request': request, 'is_user': True})
+    my_access_token = request.cookies.get(config.JWT_ACCESS_COOKIE_NAME)
+    if my_access_token:
+        is_user = True
+    else:
+        is_user = False
+    return templates.TemplateResponse(request, 'home.html', {'request': request, 'is_user': is_user})
 
 @front.get('/account')
-async def account(request: Request):
-    return templates.TemplateResponse(request, 'account.html', {'request': request})
+async def account(request: Request, username = Cookie(...)):
+    return templates.TemplateResponse(request, 'account.html', {'request': request, 'user': username})
 
-@front.get('/token')
-async def token(my_access_token = Cookie(), user_data = Cookie()):
-    return 'window.location.href = "/login"'
+
 #!BACK
 @front.post('/api/check-user')
 async def check_user(request: Request, response: Response, username: str = Body(embed=True, min_length=5), password: str = Body(embed=True, min_length=8)):
-        user = {'username': username, 'password': password}
+        class User:
+            def __init__(self, username, password):
+                self.username = username
+                self.password = password
+                self.id = 0
 
-        if user and user['password'] == password:
-            access_token = authx.create_access_token(uid='0')
+        user = User(username, password)        
+
+        if user and user.password == password:
+            access_token = authx.create_access_token(uid=str(user.id))
 
             response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, access_token)
-            response.set_cookie('user_data', user)
+            response.set_cookie('username', user.username)
             return {'access_token': access_token}
         return Response(status_code=404)
 
